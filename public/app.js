e47338fe72085c89435719aa378dc84dc89cc1fb
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-06 14:55:44 +0000

    intial revision

---
+++ public/app.js
@@ -0,0 +1,13 @@
+var init = function(container, center, zoom){
+  var container = document.getElementById('main-map');
+  var center = {lat: 57.311606, lng: -2.153835}
+  var center2 = {lat: 57.312962, lng: -2.157912}
+  var mainMap = new MapWrapper(container, center, 16);
+  mainMap.addMarker(center);
+  mainMap.addMarker(center2);
+
+
+}
+
+
+window.onload = init;
