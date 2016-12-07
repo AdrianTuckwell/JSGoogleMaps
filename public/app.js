57e501291dc0cddd8027e0978bfda610c0fcfff1
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-07 08:59:48 +0000

    added lat and long to the info windows

--- public/app.js
+++ public/app.js
@@ -1,12 +1,13 @@
 var init = function(container, center, zoom){
   var container = document.getElementById('main-map');
-  var center = {lat: 57.311606, lng: -2.153835};
-  var center2 = {lat: 57.312962, lng: -2.157912};
-  var infoString = "Udny Station";
-  var infoString2 = "Tarty Burn";
-  var mainMap = new MapWrapper(container, center, 16);
-  mainMap.addMarker(center, infoString);
-  mainMap.addMarker(center2, infoString2);
+  var udnyStation = {lat: 57.311606, lng: -2.153835};
+  var tartyBurn = {lat: 57.312962, lng: -2.157912};
+  var infoString = "Udny Station: Lat: " + udnyStation.lat + " Lng: " +udnyStation.lng;
+  var infoString2 = "Tarty Burn: Lat: " + tartyBurn.lat + " Lng: " +tartyBurn.lng;
+  var mainMap = new MapWrapper(container, udnyStation, 16);
+  
+  mainMap.addMarker(udnyStation, infoString);
+  mainMap.addMarker(tartyBurn, infoString2);
   mainMap.addClickEvent();
 
   var button = document.querySelector('button');
@@ -14,12 +15,11 @@ var init = function(container, center, zoom){
   button.onclick = handleButtonClick;
 }
 
-
-
 var handleButtonClick = function() {
   var container = document.getElementById('main-map');
+  // center on inverness
   var center = {lat: 57.4778, lng: -4.2247};
-   var mainMap = new MapWrapper(container, center, 10);  
+  var mainMap = new MapWrapper(container, center, 10);  
 }
 
 window.onload = init;
